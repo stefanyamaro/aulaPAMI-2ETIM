8507e3194e677fe1a71553171debce09b64ae874
stefanyamaro <vstefany341@gmail.com>
2024-11-01 09:43:13 -0300

    Aula 01-11

--- App.js
+++ App.js
@@ -2,7 +2,8 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 import Constants from 'expo-constants';
-import Exemplo1 from './src/ex_01';
+import Exemplo1 from './src/exemplos/ex_01';
+import Exemplo2 from './src/exemplos/ex_02';
 
 import Atividade1 from './src/atividades/atv_01';
 
@@ -10,7 +11,7 @@ import Atividade1 from './src/atividades/atv_01';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Atividade1/>
+      <Exemplo2/>
       <StatusBar style="auto" />
     </View>
   );
--- src/atividades/atv_01/index.js
+++ src/atividades/atv_01/index.js
@@ -11,3 +11,4 @@ export default function Atividade1() {
         </View>
     );
 } 
+  
